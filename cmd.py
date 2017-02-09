8b200d52ee365fa40989c1cf389bbd00347c6fc8
arch-master <archmaster@yahoo.com>
2017-02-09 18:29:54 -0500

    Add exit command

--- cmd.py
+++ cmd.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 version = "0.1"
 
+import sys
 import os
 os.system("clear");
 print("******************** eBooker v" + version + " ********************")
 print("")
 
-helpString = "eBooker v" + version + " Help\n==============" + ("=" * len(version)) + "\nOptions:\nhelp - show this help\nexit - quit this session"
+helpString = "eBooker v" + version + " Help\n==============" + ("=" * len(version)) + "\nhelp - show this help\nexit - quit this session"
 
 while True:
 	cmd = str(input("ebooker > "))
 	if cmd == "help":
 		print(helpString)
+	elif cmd == "exit":
+		exitloopBool = True
+		while exitloopBool:
+			exitBool = str(input("Would you like to quit? (y/n) "))
+			if exitBool == "y":
+				exitloopBool = False
+				os.system("clear")
+				sys.exit()
+			elif exitBool == "n":
+				exitloopBool = False
+				print("OK, not exited!")
+			else:
+				print("Please type in \"y\" or  \"n\".")
 	else:
-		print("That is not a valid command.")
+		print("\"" + cmd + "\" is not a valid command. Type \"help\" for more options")
