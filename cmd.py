5ce359f7c29843bc5cc51ba831c7750fd181a77e
arch-master <archmaster@yahoo.com>
2017-02-11 08:42:09 -0500

    Comment out curses lines

--- cmd.py
+++ cmd.py
@@ -4,11 +4,13 @@ version = "0.1"
 from time import sleep
 import sys
 import os
+"""
 try:
 	import curses
 except ImportError:
 	print("Hmmm... I wonder where \"curses\" is! If the program is still running, please stop it with ^C. Please contact the developer at archmaster@yahoo.com. Please type \"51189819\" into the subject box (without the quotes)!")
 	sleep(2)
+"""
 os.system("clear")
 print("Loading...")
 sleep(3)
@@ -19,10 +21,11 @@ sleep(1)
 
 helpString = "eBooker v" + version + " Help\n==============" + ("=" * len(version)) + "\nhelp - show this help\nexit - quit this session\nabout - read about this tool\nedit - edit a file\nclear -  clear the screen"
 aboutString = "eBooker\nA command-line tool written in Python for writing Kindle eBooks. So far, it will just execute simple commands like \"help\" and \"exit.\""
+"""
 def editor(stdscr):
 	stdscr.clear()
 	stdscr.addstr("Testing...")
-
+"""
 while True:
 	cmd = str(input("ebooker > "))
 	if cmd == "help":
@@ -54,15 +57,17 @@ while True:
 				print("You want to edit an existing file!")
 			else:
 				print("Please type in \"y\" or  \"n\".")
+"""
 			stdscr = curses.initscr()
 			curses.noecho()
 			curses.cbreak()
 			stdscr.keypad(True)
-			curses.wrapper(editor)
+			editor(stdscr)
 			curses.nocbreak()
 			stdscr.keypad(False)
 			curses.echo()
 			stdscr = None;
+"""
 	elif cmd == "clear":
 		print("Clearing...")
 		sleep(2)
