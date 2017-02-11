0c1e74c091c491ce4194014b416f66218a389e3e
arch-master <archmaster@yahoo.com>
2017-02-11 12:18:40 -0500

    Change README, change version, add file creating capability

--- cmd.py
+++ cmd.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-version = "0.1"
+version = "0.3"
 
 from time import sleep
 import sys
@@ -13,7 +13,7 @@ print("")
 sleep(1)
 
 helpString = "eBooker v" + version + " Help\n==============" + ("=" * len(version)) + "\nhelp - show this help\nexit - quit this session\nabout - read about this tool\nedit - edit a file\nclear -  clear the screen"
-aboutString = "eBooker\nA command-line tool written in Python for writing Kindle eBooks. So far, it will just execute simple commands like \"help\" and \"exit.\""
+aboutString = "eBooker\nA command-line tool written in Python for writing Kindle eBooks. So far, it will just execute simple commands like \"help\" and \"exit.\" It can also create a file using the \"edit\" command."
 while True:
 	cmd = str(input("ebooker > "))
 	if cmd == "help":
@@ -43,12 +43,12 @@ while True:
 				nameblankBool = True
 				while nameblankBool:
 					newfileString = input("What would you like it to be called? ")
-					if (newfileString == "") || (newfileString == None) || (newfileString == " "):
+					if (newfileString == "") or (newfileString == None) or (newfileString == " "):
 						print("You must enter a filename.")
 					else:
 						print("Creating file...")
 						sleep(3)
-						open(x, newfileString).close()
+						open(newfileString, "a").close()
 						print("Your file is created!")
 						nameblankBool = False
 			elif editBool == "n":
