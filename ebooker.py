17df187e62e45ddf45d6b81554facf52cf3e0e65
vikashanumegowda <hanmego@usc.edu>
2017-07-05 23:56:29 -0700

    Formatted the output in help menu

--- ebooker.py
+++ ebooker.py
@@ -20,8 +20,14 @@ else:
 ##########################################################################
 __version__ = "1.1.0"
 __help_string__ = "eBooker v" + __version__ + " Help\n==============" + \
-    ("=" * len(__version__)) + "\nhelp - show this help\nexit - quit the session\nabout - read about this tool\nedit - edit/create a file\nclear -\
-      clear the screen\ndebug - give you a list of commonly occurring issues\nserve - open your book in a web browser for reviewing"
+    ("=" * len(__version__)) + "\n"+\
+    "help   - show this help\n"+\
+    "exit   - quit the session\n"+\
+    "about  - read about this tool\n"+\
+    "edit   - edit/create a file\n"+\
+    "clear  - clear the screen\n"+\
+    "debug  - give you a list of commonly occurring issues\n"+\
+    "serve  - open your book in a web browser for reviewing"
 __about_string__ = "eBooker is a command-line tool which lets you create ebooks and other text files from command line with ease. \
 You don't have to be a programming expert or a nerd to use this. Anyone with a basic knowledge in computers can use this tool very easily."
 ##########################################################################
