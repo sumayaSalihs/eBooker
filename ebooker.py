9a2f7acabf6dbf2a198491e74711c62a2d2e6929
arch-master <archmaster@yahoo.com>
2017-08-28 15:47:16 -0400

    Remove the annoying hashtags

--- ebooker.py
+++ ebooker.py
@@ -14,7 +14,6 @@ except:
 # test if files exist
 Tester().test()
 
-##########################################################################
 __version__ = "1.1.0"
 __help_string__ = "eBooker v" + __version__ + " Help\n==============" + \
     ("=" * len(__version__)) + "\n"+\
@@ -27,8 +26,6 @@ __help_string__ = "eBooker v" + __version__ + " Help\n==============" + \
     "serve  - open your book in a web browser for reviewing"
 __about_string__ = "eBooker is a command-line tool which lets you create ebooks and other text files from command line with ease. \
 You don't have to be a programming expert or a nerd to use this. Anyone with a basic knowledge in computers can use this tool very easily."
-##########################################################################
-
 
 def install_markdown():
     ''' install markdown module if not found '''
@@ -100,9 +97,6 @@ def internet():
 
 def markdown_installed():
     return 'markdown' in sys.modules
-
-
-##########################################################################
 
 clear()
 
