662d25cffced159188fc604bcce919e2b3a39870
Arch Master <archmaster@yahoo.com>
2017-12-24 13:46:16 -0600

    Update files.py

--- files.py
+++ files.py
@@ -1,3 +1,4 @@
+"""
 About
 -----
 This will currently be pseudo-code.
@@ -32,21 +33,19 @@ Home directory:
 
 Pseudo-code
 -----------
-Find user home dir
-Does ebooker.db exist?
-Yes:
-    Load it
-No:
-    Create it; loop
-Does eBooker Projects exist?
-Yes:
-    Look for projects
-    If some list them and give the option to open one
-    Ask user if they want to create a new project
-    Load regular ebooker.py but inside project folder
-No:
-    Create it; loop
-    
+"""
+#Find user home dir
+#Does ebooker.db exist?
+#Yes:
+#    Create it; loop
+#Yes:
+#    Look for projects
+#    If some list them and give the option to open one
+#    Ask user if they want to create a new project
+#    Load regular ebooker.py but inside project folder
+#No:
+#    Create it; loop
+"""
 Within ebooker
 --------------
 Add info to log file whenever something happens.
@@ -56,3 +55,4 @@ Note
 Eventually, after I have replaced this with real code, I will write the rest of this pseudo code.
 And after that-- of course-- I will replace THAT with real code.
 And then all I have to do is continue sending Amazon annoyed emails :P
+"""
